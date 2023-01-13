0d4a41e7ea5f260cf457af911c9b4ee354d99995
Yurii Kyrylov <ykyrylov@deloitte.com>
2023-01-13 07:20:04 +0100

    solution for polidrome task

--- 01_polidrom.js
+++ 01_polidrom.js
@@ -1,26 +1,6 @@
-function polindrom(str) {
-    str = str.toLowerCase()
-
-    let str2 = str.split('')
-    str2 = str2.reverse()
-    str2 = str2.join('')
-
-    if (str2 === str) {
-        return true
-    } else {
-        return false
-    }
-
-}
-
-console.log(polindrom('abba1'))
-
-
-const polindrom2 = str => {
-    str = str.toLowerCase().split(' ').join('')
-    console.log('test', str)
+const palindrome = (str) => {
+    str.toLowerCase()
     return str === str.split('').reverse().join('')
-
 }
 
-console.log(polindrom2('a     b ba  '))
+palindrome('abba')
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # jstasks
 Classic tasks Javascript
+
+
+### 01 Polidrom
+
+Given a string; you need to write a function that allows you to return true if the string is a palindrome, 
+and false otherwise. In this case, spaces and punctuation marks must be taken into account.
+
+
